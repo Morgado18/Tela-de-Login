dea90de5b60c34e9fe8f264df52c33483c0dfa11
Morgado Add <113382227+Morgado18@users.noreply.github.com>
2022-11-06 13:30:53 -1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function enviar() {
+  var name = document.querySelector('#username').value;
+  var pass = document.querySelector('#password').innerHTML;
+
+  if(name=""){
+    document.getElementById("good").innerHTML="text"
+  }
+
+}
